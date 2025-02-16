9fbb21548754d5e42456b9608708294e79174fea
Mehroz Ali <mehrozali689@gmail.com>
2025-02-16 18:37:21 +0500

    deployement

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:3000",  // ✅ Specific frontend origin
+  origin: "outrageous-sisile-mehrozali-f62a59fb.koyeb.app/",  // ✅ Specific frontend origin
   credentials: true,  // ✅ Allow cookies
   }))
 app.options("*", cors());  // ✅ Allow preflight requests
